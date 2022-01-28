f318dc4ccaf6d2e5097c24f46ae7da5ee4e978a8
Niels Heidbrink <niels.heidbrink@gmail.com>
2022-01-28 19:43:48 +0000

    Added import command

--- handlers/import.go
+++ handlers/import.go
@@ -1,8 +1,76 @@
 package handlers
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
 func startImport(uid string, source string, dryRun bool) {
-	println("Running import with:")
-	println("uid:", uid)
-	println("source:", source)
-	println("dry-run:", dryRun)
+	log.Printf("Importing transactions and schedules from file %v to user %v.\n", source, uid)
+
+	println()
+
+	log.Printf("Reading %v...\n", source)
+	export := readFile(source)
+	log.Printf("Read %v. Found %v transactions and %v schedules.\n", source, len(export.Transactions), len(export.Schedules))
+
+	println()
+
+	log.Printf("Importing %v transactions...\n", len(export.Transactions))
+	writeTransactions(uid, export.Transactions, dryRun)
+	log.Printf("Imported %v transactions.\n", len(export.Transactions))
+
+	println()
+
+	log.Printf("Importing %v schedules...\n", len(export.Schedules))
+	writeSchedules(uid, export.Schedules, dryRun)
+	log.Printf("Imported %v schedules.\n", len(export.Schedules))
+}
+
+func readFile(source string) FileExport {
+	jsonFile, err := os.Open(source)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	var export FileExport
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &export)
+
+	return export
+}
+
+func writeTransactions(uid string, documents []TransactionExport, dryRun bool) {
+	for _, document := range documents {
+		document.UID = uid
+
+		if !dryRun {
+			ref, _, err := FirestoreClient.Collection("transactions").Add(Context, document.toMap())
+
+			if err != nil {
+				log.Fatal(err.Error())
+			} else {
+				log.Printf("Created transaction with ID: %v", ref.ID)
+			}
+		}
+	}
+}
+
+func writeSchedules(uid string, documents []ScheduleExport, dryRun bool) {
+	for _, document := range documents {
+		document.UID = uid
+
+		if !dryRun {
+			ref, _, err := FirestoreClient.Collection("schedules").Add(Context, document.toMap())
+
+			if err != nil {
+				log.Fatal(err.Error())
+			} else {
+				log.Printf("Created schedule with ID: %v", ref.ID)
+			}
+		}
+	}
 }
